9a6ab6c0360a8b1817259c5852354d087635be40
SooryaBalaji <soorya.balajivijayaraghavan@gmail.com>
2026-04-11 11:02:56 -0700

    changed port for deployment

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def hardware_results():
         return jsonify({"error": "Hardware results not available yet"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=False)
